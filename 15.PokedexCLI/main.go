a915066333e03342edafd9065d4f70a602fc0d47
Ashraful Islam <ashraful.meche@gmail.com>
2024-02-09 14:24:37 -0600

    initial setup for bubble tea tui for pokedex

--- 15.PokedexCLI/main.go
+++ 15.PokedexCLI/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"math/rand"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/1-ashraful-islam/boot.dev-projects/15.PokedexCLI/internal/pokeapi"
 	"github.com/1-ashraful-islam/boot.dev-projects/15.PokedexCLI/internal/pokecache"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type appConfig struct {
@@ -24,7 +24,7 @@ type appConfig struct {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(cf *appConfig, args []string) error
+	callback    func(cf *appConfig, args []string) (string, error)
 }
 
 func getCliCommands() map[string]cliCommand {
@@ -72,92 +72,92 @@ func getCliCommands() map[string]cliCommand {
 	}
 }
 
-func commandHelp(_ *appConfig, _ []string) error {
+func commandHelp(_ *appConfig, _ []string) (string, error) {
 	help_message := "\nWelcome to the Pokedex!\nUsages:\n"
 	commands := getCliCommands()
 	for _, command := range commands {
 		help_message += fmt.Sprintf("  %s: %s\n", command.name, command.description)
 	}
-	fmt.Println(help_message)
-	return nil
+	return help_message, nil
 }
 
-func commandExit(_ *appConfig, _ []string) error {
-	fmt.Println("Goodbye!")
-	return nil
+func commandExit(_ *appConfig, _ []string) (string, error) {
+	return "Goodbye!", nil
 }
 
-func commandMap(cf *appConfig, _ []string) error {
+func commandMap(cf *appConfig, _ []string) (string, error) {
 	if cf.Next == nil {
-		return fmt.Errorf("no more location areas")
+		return "", fmt.Errorf("no more location areas")
 	}
 	locations, err := pokeapi.GetLocationArea(*cf.Next, cf.Cache)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	cf.Next = locations.Next
 	cf.Previous = locations.Previous
 
+	result := ""
 	for _, location := range locations.Results {
-		fmt.Println(location.Name)
+		result += location.Name + "\n"
 	}
-	return nil
+	return result, nil
 }
 
-func commandMapb(cf *appConfig, _ []string) error {
+func commandMapb(cf *appConfig, _ []string) (string, error) {
 	if cf.Previous == nil {
-		return fmt.Errorf("no previous location areas")
+		return "", fmt.Errorf("no previous location areas")
 	}
 	locations, err := pokeapi.GetLocationArea(*cf.Previous, cf.Cache)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	cf.Next = locations.Next
 	cf.Previous = locations.Previous
 
+	result := ""
 	for _, location := range locations.Results {
-		fmt.Println(location.Name)
+		result += location.Name + "\n"
 	}
 
-	return nil
+	return result, nil
 }
 
-func commandExplore(cf *appConfig, args []string) error {
+func commandExplore(cf *appConfig, args []string) (string, error) {
 	const exploreBaseURL = "https://pokeapi.co/api/v2/location-area/"
 
 	if len(args) < 2 {
-		return fmt.Errorf("explore command requires a valid location area name")
+		return "", fmt.Errorf("explore command requires a valid location area name")
 	}
 
-	fmt.Printf("Exploring %v ...\n", args[1])
+	result := fmt.Sprintf("Exploring %v ...\n", args[1])
 
 	exploreURL := exploreBaseURL + args[1] + "/"
 	encounters, err := pokeapi.GetPokemonInLocationArea(exploreURL, cf.Cache)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	fmt.Println("Found Pokémon:")
+	result += "Found Pokémon:\n"
 	for _, encounter := range encounters.PokemonEncounters {
-		fmt.Println(" - ", encounter.Pokemon.Name)
+		result += " - " + encounter.Pokemon.Name + "\n"
 	}
 
-	return nil
+	return result, nil
 }
 
-func commandCatch(cf *appConfig, args []string) error {
+func commandCatch(cf *appConfig, args []string) (string, error) {
 	const pokemonBaseURL = "https://pokeapi.co/api/v2/pokemon/"
 
 	if len(args) < 2 {
-		return fmt.Errorf("catch command requires a valid Pokémon name")
+		return "", fmt.Errorf("catch command requires a valid Pokémon name")
 	}
-	fmt.Printf("Throwing a Pokéball at %s...\n", args[1])
+	result := fmt.Sprintf("Throwing a Pokéball at %s...\n", args[1])
 	pokemonURL := pokemonBaseURL + args[1] + "/"
 	pokemon, err := pokeapi.GetPokemon(pokemonURL, cf.Cache)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	//assuming max base experience is 608 for a Pokémon currently
@@ -172,21 +172,21 @@ func commandCatch(cf *appConfig, args []string) error {
 			"Almost had it!",
 			fmt.Sprintf("%s escaped!", pokemon.Name),
 		}
-		fmt.Println(fail_phrases[rand.Intn(len(fail_phrases))])
-		return nil
+		result += fail_phrases[rand.Intn(len(fail_phrases))]
+		return result, nil
 	}
-	fmt.Printf("Caught %s!\n", pokemon.Name)
+	result += fmt.Sprintf("Caught %s!\n", pokemon.Name)
 	//show the Pokémon caught
 	for _, line := range pokemon.ASCIIArt {
-		fmt.Println(line)
+		result += line + "\n"
 	}
 	cf.Pokedex[pokemon.Name] = *pokemon
-	return nil
+	return result, nil
 }
 
-func commandInspect(cf *appConfig, args []string) error {
+func commandInspect(cf *appConfig, args []string) (string, error) {
 	if len(args) < 2 {
-		return fmt.Errorf(("inspect command requires a valid Pokémon name"))
+		return "", fmt.Errorf("inspect command requires a valid Pokémon name")
 	}
 	if pokemon, ok := cf.Pokedex[args[1]]; ok {
 
@@ -204,18 +204,7 @@ func commandInspect(cf *appConfig, args []string) error {
 			pokemonStats = append(pokemonStats, fmt.Sprintf("  - %s", t.Type.Name))
 		}
 
-		// fmt.Println("Name:", pokemon.Name)
-		// fmt.Println("Height:", pokemon.Height)
-		// fmt.Println("Weight:", pokemon.Weight)
-		// fmt.Println("Stats:")
-		// for _, stat := range pokemon.Stats {
-		// 	fmt.Printf("  - %v: %d\n", stat.Stat.Name, stat.BaseStat)
-		// }
-		// fmt.Println("Types:")
-		// for _, t := range pokemon.Types {
-		// 	fmt.Println("  -", t.Type.Name)
-		// }
-
+		result := ""
 		for i := 0; i < len(pokemonStats) || i < len(pokemon.ASCIIArt); i++ {
 			stat := ""
 			if i < len(pokemonStats) {
@@ -225,8 +214,9 @@ func commandInspect(cf *appConfig, args []string) error {
 			if i < len(pokemon.ASCIIArt) {
 				art = pokemon.ASCIIArt[i]
 			}
-			fmt.Printf("%-30s %s\n", stat, art)
+			result += fmt.Sprintf("%-30s %s\n", stat, art)
 		}
+		return result, nil
 	} else {
 		fail_phrases := []string{
 			"Who's that Pokémon?",
@@ -234,12 +224,11 @@ func commandInspect(cf *appConfig, args []string) error {
 			"You need to catch the Pokémon first!",
 			fmt.Sprintf("You have not caught %s yet!", args[1]),
 		}
-		fmt.Println(fail_phrases[rand.Intn(len(fail_phrases))])
+		return "", fmt.Errorf(fail_phrases[rand.Intn(len(fail_phrases))])
 	}
-	return nil
 }
 
-func commandPokedex(cf *appConfig, _ []string) error {
+func commandPokedex(cf *appConfig, _ []string) (string, error) {
 	if len(cf.Pokedex) == 0 {
 		fail_phrases := []string{
 			"Pokedéx is empty! Go catch some Pokémon!",
@@ -247,10 +236,9 @@ func commandPokedex(cf *appConfig, _ []string) error {
 			"Your Pokédex is empty!",
 			"Your Pokédex is empty! Go catch 'em all!",
 		}
-		fmt.Println(fail_phrases[rand.Intn(len(fail_phrases))])
-		return nil
+		return "", fmt.Errorf(fail_phrases[rand.Intn(len(fail_phrases))])
 	}
-	fmt.Println("Your Pokédex:")
+	result := "Your Pokédex:\n"
 	pokedex := make([]string, 0)
 	for name := range cf.Pokedex {
 		pokedex = append(pokedex, name)
@@ -258,19 +246,19 @@ func commandPokedex(cf *appConfig, _ []string) error {
 	for i := 0; i < len(pokedex); i += 2 {
 		//2 columns
 		for j := i; j < i+2 && j < len(pokedex); j++ {
-			fmt.Printf(" - %-40s", pokedex[j])
+			result += fmt.Sprintf(" - %-40s", pokedex[j])
 		}
-		fmt.Println()
+		result += "\n"
 		for k := 0; k < len(cf.Pokedex[pokedex[i]].ASCIIArt); k++ {
 			for j := i; j < i+2 && j < len(pokedex); j++ {
-				fmt.Printf("   %-43s", cf.Pokedex[pokedex[j]].ASCIIArt[k])
+				result += fmt.Sprintf("   %-43s", cf.Pokedex[pokedex[j]].ASCIIArt[k])
 			}
-			fmt.Println()
+			result += "\n"
 		}
-		fmt.Println()
+		result += "\n"
 
 	}
-	return nil
+	return result, nil
 }
 
 func main() {
@@ -287,29 +275,108 @@ func main() {
 		Pokedex:  make(map[string]pokeapi.Pokemon),
 	}
 
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Pokedex > ")
-	for scanner.Scan() {
-		args := strings.Split(scanner.Text(), " ")
-		command, ok := commands[args[0]]
-		if ok {
-			if err := command.callback(config, args); err != nil {
-				fmt.Println("Error:", err)
-			}
-			if command.name == "exit" {
-				break
-			}
-		} else {
-			fmt.Printf("Unknown command: %s\n", args[0])
-			commands["help"].callback(nil, nil)
-		}
-		fmt.Print("\nPokedex > ")
+	startingScreen := &MainScreen{
+		config:      config,
+		cliCommands: commands,
+		cmds:        []string{"map", "mapb", "explore", "catch", "inspect", "pokedex", "exit"},
+		cursor:      0,
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Reading standard input:", err)
+	app := tea.NewProgram(&appModel{
+		config:        config,
+		cliCommands:   commands,
+		screens:       make(map[string]UIScreen),
+		currentScreen: startingScreen,
+	})
+
+	if _, err := app.Run(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 
 	cancel()
 	//add a small delay to allow the go routines to exit gracefully
 	time.Sleep(10 * time.Millisecond)
+}
+
+// interface to separate UI screen or context
+type UIScreen interface {
+	Update(msg tea.Msg) (UIScreen, tea.Cmd)
+	View() string
+}
+
+type MainScreen struct {
+	config      *appConfig
+	cliCommands map[string]cliCommand
+	cmds        []string
+	cursor      int
+}
+
+func (m *MainScreen) Update(msg tea.Msg) (UIScreen, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc":
+			return m, tea.Quit
+			// left keys
+		case "left":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		// right keys
+		case "right":
+			if m.cursor < len(m.cmds)-1 {
+				m.cursor++
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m *MainScreen) View() string {
+	var s string
+
+	cmdHelp := ""
+	//main menu
+	for i, command := range m.cmds {
+		cursorBegin := " "
+		cursorEnd := " "
+
+		if m.cursor == i {
+			cursorBegin = "<"
+			cursorEnd = ">"
+			cmdHelp = m.cliCommands[command].description
+		}
+		s += fmt.Sprintf(" %s%s%s ", cursorBegin, command, cursorEnd)
+	}
+
+	s += "\n\n" + cmdHelp
+
+	//footer
+	s += "\nPress 'q' or 'esc' to quit.\n"
+	return "Welcome to Pokédex!\n" + s
+}
+
+type appModel struct {
+	config        *appConfig
+	cliCommands   map[string]cliCommand
+	screens       map[string]UIScreen
+	currentScreen UIScreen
+	prevScreen    UIScreen
+}
+
+// Init implements tea.Model.
+func (m *appModel) Init() tea.Cmd {
+	return nil
+}
+
+// delegate the update to the current screen
+func (m *appModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.currentScreen, cmd = m.currentScreen.Update(msg)
+	return m, cmd
+}
+
+// delegate the view to the current screen
+func (m *appModel) View() string {
+	return m.currentScreen.View()
 }
