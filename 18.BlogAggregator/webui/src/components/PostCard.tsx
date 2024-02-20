63766b06acb6a9b617213a83155f4678e6fe7123
Ashraful Islam <ashraful.meche@gmail.com>
2024-02-20 03:08:08 -0600

    blog aggregator: webui: adds date and numeric id for posts

--- 18.BlogAggregator/webui/src/components/PostCard.tsx
+++ 18.BlogAggregator/webui/src/components/PostCard.tsx
@@ -1,16 +1,29 @@
 import { FaLink } from "react-icons/fa";
 import styles from "./PostCard.module.css";
+import { format } from "date-fns";
 
 export interface Post {
   id: string;
   title: string;
   url: string;
   description: string;
+  publish_date: string;
 }
 
-export default function PostCard({ post }: { post: Post }) {
+export default function PostCard({
+  post,
+  index,
+}: {
+  post: Post;
+  index: number;
+}) {
+  const formattedDate = format(post.publish_date, "dd MMM yyyy");
   return (
     <div className={styles.postCardContainer}>
+      <div className={styles.flexContainer}>
+        <span>{formattedDate}</span>{" "}
+        <span className={styles.numericBadge}>{index + 1}</span>
+      </div>
       <h4>
         {post.title || "Post Title"}{" "}
         <a href={post.url || "#"} target="_blank" rel="noreferrer">
